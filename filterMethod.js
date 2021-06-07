5912063525584b7d7f64d513a5bb75a60a89d1b5
jordanvillacorta <jordan.villacorta@gmail.com>
2021-06-07 15:08:39 -0400

    added filterMethod example to Documentation

---
+++ filterMethod.js
@@ -0,0 +1,16 @@
+let superHeroes = [
+    { name: "Robert Downey, Jr.",  hero: "Iron Man" },
+    { name: "Chris Evans", hero: "Captain America" },
+    { name: "Mark Ruffalo", hero: "Hulk"   },
+    { name: "Tom Hiddleston", hero: "Loki" }
+];
+
+let newArray = superHeroes.filter(function (element) {
+    return (element.hero === "Captain America");
+}).map(function(element) {
+return element.name;
+}).sort()
+
+console.log(newArray);
+
+// ["Chris Evans"]
