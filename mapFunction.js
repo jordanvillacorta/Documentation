cd4be9ac079520a9947957fb459ed692a24a0c56
jordanvillacorta <jordan.villacorta@gmail.com>
2021-06-11 23:40:04 -0400

    added mapFunction example to Documentation

--- mapFunction.js
+++ mapFunction.js
@@ -14,3 +14,14 @@ newArr = [
   {value: 3, index: 2},
   {value: 4, index: 3}
 ]
+
+// In this example we have a simple array:
+let arr = [1,2,3,4];
+
+// We want to double the even numbers and leave the odd numbers the same. In order to accomplish this, we can add logic within our map() function:
+
+let newArr = arr.map((v,i,a) => {
+  return v % 2 === 0 ? v * 2 : v;
+});
+
+// newArr = [1,4,3,8];
