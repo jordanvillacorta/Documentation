15d321faafd9877875d8bdac5cec64faeac7019e
jordanvillacorta <jordan.villacorta@gmail.com>
2021-06-13 22:54:59 -0400

    added revised examples to mapFunction

--- mapFunction.js
+++ mapFunction.js
@@ -16,12 +16,22 @@ newArr = [
 ]
 
 // In this example we have a simple array:
+
 let arr = [1,2,3,4];
 
 // We want to double the even numbers and leave the odd numbers the same. In order to accomplish this, we can add logic within our map() function:
 
 let newArr = arr.map((v,i,a) => {
   return v % 2 === 0 ? v * 2 : v;
+});
+
+// Same map function but in ES5 I think
+let newArr = arr.map((v,i,a) => {
+  if (v % 2 === 0){
+    return v * 2;
+  } else {
+    return v;
+  }
 });
 
 // newArr = [1,4,3,8];
