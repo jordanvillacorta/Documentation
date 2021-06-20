c968dd1d0bd8e291cf99f06e964d97f0549c590b
Jordan Villacorta <jvillacortadla@gmail.com>
2021-06-19 22:42:03 -0400

    Added String.prototype.repeat() method from MDN to Documentation

---
+++ repeatMethod.js
@@ -0,0 +1,6 @@
+The repeat() method constructs and returns a new string which contains the specified number of copies of the string on which it was called, concatenated together.
+const chorus = 'Because I\'m happy. ';
+
+console.log(`Chorus lyrics for "Happy": ${chorus.repeat(27)}`);
+
+// expected output: "Chorus lyrics for "Happy": Because I'm happy. Because I'm happy.
